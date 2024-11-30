6c714c1dc89a4464891f29dada641462b306522d
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2024-11-30 23:10:42 +0100

    Protecting home page

--- configs/routes/web.php
+++ configs/routes/web.php
@@ -5,14 +5,15 @@ declare(strict_types = 1);
 use App\Controllers\AuthController;
 use App\Controllers\HomeController;
 use App\Middleware\AuthMiddleware;
+use App\Middleware\GuestMiddleware;
 use Slim\App;
 
 return function (App $app) {
     $app->get('/', [HomeController::class, 'index'])->add(AuthMiddleware::class);
 
-    $app->get('/login', [AuthController::class, 'loginView']);
-    $app->get('/register', [AuthController::class, 'registerView']);
-    $app->post('/login', [AuthController::class, 'logIn']);
-    $app->post('/register', [AuthController::class, 'register']);
+    $app->get('/login', [AuthController::class, 'loginView'])->add(GuestMiddleware::class);
+    $app->get('/register', [AuthController::class, 'registerView'])->add(GuestMiddleware::class);
+    $app->post('/login', [AuthController::class, 'logIn'])->add(GuestMiddleware::class);
+    $app->post('/register', [AuthController::class, 'register'])->add(GuestMiddleware::class);
     $app->get('/logout', [AuthController::class, 'logOut'])->add(AuthMiddleware::class);
 };
