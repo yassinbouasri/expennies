a65155ff5e97c3599aaf0324bf6cecd835e42830
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2024-12-01 12:20:35 +0100

    Passing authed user in the request

--- app/Auth.php
+++ app/Auth.php
@@ -7,6 +7,7 @@ namespace App;
 use App\Contracts\AuthInterface;
 use App\Contracts\UserInterface;
 use App\Entity\User;
+use App\Exception\ValidationException;
 use Doctrine\ORM\EntityManager;
 
 class Auth implements AuthInterface
@@ -36,6 +37,33 @@ class Auth implements AuthInterface
         }
         $this->user = $user;
         return $this->user;
+
+    }
+
+    public function attemptLogin(array $credentials): bool
+    {
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $credentials['email']]);
+        if (! $user || ! $this->checkCrdentials($user, $credentials)) {
+            return false;
+        }
+
+        session_regenerate_id();
+
+        $_SESSION['user'] = $user->getId();
+        $this->user = $user;
+
+        return true;
+    }
+
+    public function checkCrdentials(UserInterface $user, array $credentials): bool
+    {
+        return password_verify($credentials['password'], $user->getPassword());
+    }
+
+    public function logOut(): void
+    {
+        unset($_SESSION['user']);
+        $this->user = null;
 
     }
 }
--- app/Contracts/AuthInterface.php
+++ app/Contracts/AuthInterface.php
@@ -8,4 +8,10 @@ interface AuthInterface
 {
     public function user(): ?UserInterface;
 
+    public function attemptLogin(array $data): bool;
+
+    public function checkCrdentials(UserInterface $user, array $credentials): bool;
+
+    public function logOut(): void;
+
 }
