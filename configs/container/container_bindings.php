2231308efe9262de657ecd2406e6363257c9c00d
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2024-12-04 01:52:36 +0100

    Csrf

--- configs/container/container_bindings.php
+++ configs/container/container_bindings.php
@@ -19,6 +19,7 @@ use Doctrine\ORM\ORMSetup;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Slim\App;
+use Slim\Csrf\Guard;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Symfony\Bridge\Twig\Extension\AssetExtension;
@@ -89,5 +90,6 @@ return [
     ),
     RequestValidatorFactoryInterface::class => fn (ContainerInterface $container) => $container->get(
         RequestValidatorFactory::class
-    )
+    ),
+    'csrf' => fn(ResponseFactoryInterface $responseFactory) => new Guard($responseFactory, persistentTokenMode: true),
 ];
--- configs/middleware.php
+++ configs/middleware.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 use App\Config;
 use App\Middleware\AuthenticationMiddleware;
+use App\Middleware\CsrfFieldsMiddleware;
 use App\Middleware\OldFormDataMiddleware;
 use App\Middleware\StartSessionsMiddleware;
 use App\Middleware\ValidationErrorsMiddleware;
@@ -16,6 +17,8 @@ return function (App $app) {
     $container = $app->getContainer();
     $config    = $container->get(Config::class);
 
+    $app->add(CsrfFieldsMiddleware::class);
+    $app->add('csrf');
     $app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
     $app->add(ValidationExceptionMiddleware::class);
     $app->add(ValidationErrorsMiddleware::class);
