0ddf6e2e2313916fca1fc5397204ee60d3dd6f44
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2024-12-05 12:17:42 +0100

    Working on categories crud

--- app/Controllers/CategoriesController.php
+++ app/Controllers/CategoriesController.php
@@ -5,7 +5,21 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use Slim\Views\Twig;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
 class CategoriesController
 {
+    public function __construct(private readonly Twig $twig)
+    {
+    }
+
+    public function index(Request $request,Response $response): Response
+    {
+        return $this->twig->render($response,'categories/index.twig');
+    }
+
+
 
 }
