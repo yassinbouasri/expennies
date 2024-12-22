fa8a064bbba89a0c3847eb030ae2c67696569a5c
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2024-12-22 01:51:09 +0100

    Implementing Rate Limiting

--- configs/container/container_bindings.php
+++ configs/container/container_bindings.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 use App\Auth;
 use App\Config;
@@ -21,6 +21,7 @@ use App\RouteEntityBindingStrategy;
 use App\Services\EntityManagerService;
 use App\Services\UserProviderService;
 use App\Session;
+use Clockwork\Clockwork;
 use Clockwork\DataSource\DoctrineDataSource;
 use Clockwork\Storage\FileStorage;
 use Doctrine\DBAL\DriverManager;
@@ -47,130 +48,95 @@ use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mailer\Transport;
 use Symfony\Component\Mime\BodyRendererInterface;
+use Symfony\Component\RateLimiter\RateLimiterFactory;
+use Symfony\Component\RateLimiter\Storage\CacheStorage;
 use Symfony\WebpackEncoreBundle\Asset\EntrypointLookup;
 use Symfony\WebpackEncoreBundle\Asset\TagRenderer;
 use Symfony\WebpackEncoreBundle\Twig\EntryFilesTwigExtension;
 use Twig\Extra\Intl\IntlExtension;
-use Clockwork\Clockwork;
-
 use function DI\create;
 
-return [
-    App::class                              => function (ContainerInterface $container) {
-        AppFactory::setContainer($container);
+return [App::class => function (ContainerInterface $container) {
+    AppFactory::setContainer($container);
 
-        $addMiddlewares = require CONFIG_PATH . '/middleware.php';
-        $router         = require CONFIG_PATH . '/routes/web.php';
+    $addMiddlewares = require CONFIG_PATH . '/middleware.php';
+    $router = require CONFIG_PATH . '/routes/web.php';
 
-        $app = AppFactory::create();
+    $app = AppFactory::create();
 
-        $app->getRouteCollector()->setDefaultInvocationStrategy(
-            new RouteEntityBindingStrategy(
-                $container->get(EntityManagerServiceInterface::class),
-                $app->getResponseFactory()
-            )
-        );
+    $app->getRouteCollector()->setDefaultInvocationStrategy(new RouteEntityBindingStrategy($container->get(EntityManagerServiceInterface::class), $app->getResponseFactory()));
 
-        $router($app);
+    $router($app);
 
-        $addMiddlewares($app);
+    $addMiddlewares($app);
 
-        return $app;
-    },
-    Config::class                           => create(Config::class)->constructor(
-        require CONFIG_PATH . '/app.php'
-    ),
-    EntityManagerInterface::class           => function (Config $config) {
-        $ormConfig = ORMSetup::createAttributeMetadataConfiguration(
-            $config->get('doctrine.entity_dir'),
-            $config->get('doctrine.dev_mode')
-        );
+    return $app;
+}, Config::class => create(Config::class)->constructor(require CONFIG_PATH . '/app.php'), EntityManagerInterface::class => function (
+    Config $config
+) {
+    $ormConfig = ORMSetup::createAttributeMetadataConfiguration($config->get('doctrine.entity_dir'), $config->get('doctrine.dev_mode'));
 
-        $ormConfig->addFilter('user', UserFilter::class);
+    $ormConfig->addFilter('user', UserFilter::class);
 
-        return new EntityManager(
-            DriverManager::getConnection($config->get('doctrine.connection'), $ormConfig),
-            $ormConfig
-        );
-    },
-    Twig::class                             => function (Config $config, ContainerInterface $container) {
-        $twig = Twig::create(VIEW_PATH, [
-            'cache'       => STORAGE_PATH . '/cache/templates',
-            'auto_reload' => AppEnvironment::isDevelopment($config->get('app_environment')),
-        ]);
+    return new EntityManager(DriverManager::getConnection($config->get('doctrine.connection'), $ormConfig), $ormConfig);
+}, Twig::class => function (Config $config, ContainerInterface $container) {
+    $twig = Twig::create(VIEW_PATH, ['cache' => STORAGE_PATH . '/cache/templates', 'auto_reload' => AppEnvironment::isDevelopment($config->get('app_environment')),]);
 
-        $twig->addExtension(new IntlExtension());
-        $twig->addExtension(new EntryFilesTwigExtension($container));
-        $twig->addExtension(new AssetExtension($container->get('webpack_encore.packages')));
+    $twig->addExtension(new IntlExtension());
+    $twig->addExtension(new EntryFilesTwigExtension($container));
+    $twig->addExtension(new AssetExtension($container->get('webpack_encore.packages')));
 
-        return $twig;
-    },
-    /**
-     * The following two bindings are needed for EntryFilesTwigExtension & AssetExtension to work for Twig
-     */
-    'webpack_encore.packages'               => fn() => new Packages(
-        new Package(new JsonManifestVersionStrategy(BUILD_PATH . '/manifest.json'))
-    ),
-    'webpack_encore.tag_renderer'           => fn(ContainerInterface $container) => new TagRenderer(
-        new EntrypointLookup(BUILD_PATH . '/entrypoints.json'),
-        $container->get('webpack_encore.packages')
-    ),
-    ResponseFactoryInterface::class         => fn(App $app) => $app->getResponseFactory(),
-    AuthInterface::class                    => fn(ContainerInterface $container) => $container->get(
-        Auth::class
-    ),
-    UserProviderServiceInterface::class     => fn(ContainerInterface $container) => $container->get(
-        UserProviderService::class
-    ),
-    SessionInterface::class                 => fn(Config $config) => new Session(
-        new SessionConfig(
-            $config->get('session.name', ''),
-            $config->get('session.flash_name', 'flash'),
-            $config->get('session.secure', true),
-            $config->get('session.httponly', true),
-            SameSite::from($config->get('session.samesite', 'lax'))
-        )
-    ),
-    RequestValidatorFactoryInterface::class => fn(ContainerInterface $container) => $container->get(
-        RequestValidatorFactory::class
-    ),
-    'csrf'                                  => fn(ResponseFactoryInterface $responseFactory, Csrf $csrf) => new Guard(
-        $responseFactory, failureHandler: $csrf->failureHandler(), persistentTokenMode: true
-    ),
-    Filesystem::class                       => function (Config $config) {
+    return $twig;
+}, /**
+ * The following two bindings are needed for EntryFilesTwigExtension & AssetExtension to work for Twig
+ */
+    'webpack_encore.packages' => fn(
+    ) => new Packages(new Package(new JsonManifestVersionStrategy(BUILD_PATH . '/manifest.json'))), 'webpack_encore.tag_renderer' => fn(
+        ContainerInterface $container
+    ) => new TagRenderer(new EntrypointLookup(BUILD_PATH . '/entrypoints.json'), $container->get('webpack_encore.packages')), ResponseFactoryInterface::class => fn(
+        App $app
+    ) => $app->getResponseFactory(), AuthInterface::class => fn(ContainerInterface $container
+    ) => $container->get(Auth::class), UserProviderServiceInterface::class => fn(ContainerInterface $container
+    ) => $container->get(UserProviderService::class), SessionInterface::class => fn(Config $config
+    ) => new Session(new SessionConfig($config->get('session.name', ''), $config->get('session.flash_name', 'flash'), $config->get('session.secure', true), $config->get('session.httponly', true), SameSite::from($config->get('session.samesite', 'lax')))), RequestValidatorFactoryInterface::class => fn(
+        ContainerInterface $container
+    ) => $container->get(RequestValidatorFactory::class), 'csrf' => fn(
+        ResponseFactoryInterface $responseFactory,
+        Csrf $csrf
+    ) => new Guard($responseFactory, failureHandler: $csrf->failureHandler(), persistentTokenMode: true), Filesystem::class => function (
+        Config $config
+    ) {
         $adapter = match ($config->get('storage.driver')) {
             StorageDriver::Local => new League\Flysystem\Local\LocalFilesystemAdapter(STORAGE_PATH),
         };
 
         return new League\Flysystem\Filesystem($adapter);
-    },
-    Clockwork::class                        => function (EntityManagerInterface $entityManager) {
+    }, Clockwork::class => function (EntityManagerInterface $entityManager) {
         $clockwork = new Clockwork();
 
         $clockwork->storage(new FileStorage(STORAGE_PATH . '/clockwork'));
         $clockwork->addDataSource(new DoctrineDataSource($entityManager));
 
         return $clockwork;
-    },
-    EntityManagerServiceInterface::class    => fn(EntityManagerInterface $entityManager) => new EntityManagerService(
-        $entityManager
-    ),
-    MailerInterface::class                  => function (Config $config) {
+    }, EntityManagerServiceInterface::class => fn(EntityManagerInterface $entityManager
+    ) => new EntityManagerService($entityManager), MailerInterface::class => function (Config $config) {
         $transport = Transport::fromDsn($config->get('mailer.dsn'));
 
         return new Mailer($transport);
-    },
-    BodyRendererInterface::class            => fn(Twig $twig) => new BodyRenderer($twig->getEnvironment()),
-    RouteParserInterface::class             => fn(App $app) => $app->getRouteCollector()->getRouteParser(),
-    CacheInterface::class                   => function (Config $config) {
-        $redis  = new \Redis();
+    }, BodyRendererInterface::class => fn(Twig $twig
+    ) => new BodyRenderer($twig->getEnvironment()), RouteParserInterface::class => fn(App $app
+    ) => $app->getRouteCollector()->getRouteParser(), CacheInterface::class => fn(RedisAdapter $redisAdapter
+    ) => new Psr16Cache($redisAdapter), RedisAdapter::class => function (Config $config) {
+        $redis = new Redis();
         $config = $config->get('redis');
 
-        $redis->connect($config['host'], (int) $config['port']);
+        $redis->connect($config['host'], (int)$config['port']);
         $redis->auth($config['password']);
 
-        $adapter = new RedisAdapter($redis);
 
-        return new Psr16Cache($adapter);
-    },
-];
+        return new RedisAdapter($redis);
+    }, RateLimiterFactory::class => function (RedisAdapter $redisAdapter) {
+        $storage = new CacheStorage($redisAdapter);
+
+        return new RateLimiterFactory(['id' => 'default', 'policy' => 'fixed_window', 'interval' => '1 minute', 'limit' => 3], $storage);
+    }];
--- configs/routes/web.php
+++ configs/routes/web.php
@@ -13,6 +13,7 @@ use App\Controllers\TransactionImporterController;
 use App\Controllers\VerifyController;
 use App\Middleware\AuthMiddleware;
 use App\Middleware\GuestMiddleware;
+use App\Middleware\RateLimitMiddleware;
 use App\Middleware\ValidateSignatureMiddleware;
 use App\Middleware\VerifyEmailMiddleware;
 use Slim\App;
@@ -64,20 +65,20 @@ return function (App $app) {
         $group->get('/verify/{id}/{hash}', [VerifyController::class, 'verify'])
               ->setName('verify')
               ->add(ValidateSignatureMiddleware::class);
-        $group->post('/verify', [VerifyController::class, 'resend']);
+        $group->post('/verify', [VerifyController::class, 'resend'])->setName('resendVerification')->add(RateLimitMiddleware::class);
     })->add(AuthMiddleware::class);
 
     $app->group('', function (RouteCollectorProxy $guest) {
         $guest->get('/login', [AuthController::class, 'loginView']);
         $guest->get('/register', [AuthController::class, 'registerView']);
-        $guest->post('/login', [AuthController::class, 'logIn']);
-        $guest->post('/register', [AuthController::class, 'register']);
-        $guest->post('/login/two-factor', [AuthController::class, 'twoFactorLogin']);
+        $guest->post('/login', [AuthController::class, 'logIn'])->setName('logIn')->add(RateLimitMiddleware::class);
+        $guest->post('/register', [AuthController::class, 'register'])->setName('register')->add(RateLimitMiddleware::class);
+        $guest->post('/login/two-factor', [AuthController::class, 'twoFactorLogin'])->setName('twoFactorLogin')->add(RateLimitMiddleware::class);
         $guest->get('/forgot-password', [PasswordResetController::class, 'showForgotPasswordForm']);
         $guest->get('/reset-password/{token}', [PasswordResetController::class, 'showResetPasswordForm'])
             ->setName('password-reset')
             ->add(ValidateSignatureMiddleware::class);
-        $guest->post('/forgot-password', [PasswordResetController::class, 'handleForgotPasswordRequest']);
-        $guest->post('/reset-password/{token}', [PasswordResetController::class, 'resetPassword']);
+        $guest->post('/forgot-password', [PasswordResetController::class, 'handleForgotPasswordRequest'])->setName('handleForgotPassword')->add(RateLimitMiddleware::class);
+        $guest->post('/reset-password/{token}', [PasswordResetController::class, 'resetPassword'])->setName('resetPassword')->add(RateLimitMiddleware::class);
     })->add(GuestMiddleware::class);
 };
